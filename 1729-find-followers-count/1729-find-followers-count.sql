d1a2a5018a854fa8ac55222e1526cfb6a3426b80
BHARANIDHARAN K <bharanidharan5544@gmail.com>
2026-01-28 15:54:19 +0530

    Time: 531 ms (46.96%), Space: 0B (100%) - LeetHub

--- 1729-find-followers-count/1729-find-followers-count.sql
+++ 1729-find-followers-count/1729-find-followers-count.sql
@@ -1,1 +1,1 @@
-select user_id , count(*) as followers_count from followers group by user_id order by user_id asc;
+select user_id , count(follower_id) as followers_count from followers group by user_id order by user_id asc;
