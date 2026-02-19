c3b70e0e1385b4ec1af1352945b4c75cc2273a0e
BHARANIDHARAN K <bharanidharan5544@gmail.com>
2026-02-19 12:03:30 +0530

    Time: 2622 ms (5.13%), Space: 280.5 MB (28.21%) - LeetHub

--- 0805-split-array-with-same-average/0805-split-array-with-same-average.java
+++ 0805-split-array-with-same-average/0805-split-array-with-same-average.java
@@ -1,82 +1,45 @@
-import java.awt.Point;
-
-class Solution {
-    public boolean splitArraySameAverage(int[] A) {
-        int N = A.length;
-        int S = 0;
-        for (int x: A) S += x;
-        if (N == 1) return false;
-
-        int g = gcd(S, N);
-        Point mu = new Point(-(S/g), N/g);
-        // A[i] -> fracAdd(A[i], mu)
-        List<Point> A2 = new ArrayList();
-        for (int x: A)
-            A2.add(fracAdd(new Point(x, 1), mu));
-
-        Set<Point> left = new HashSet();
-        left.add(A2.get(0));
-        for (int i = 1; i < N/2; ++i) {
-            Set<Point> left2 = new HashSet();
-            Point z = A2.get(i);
-            left2.add(z);
-            for (Point p: left) {
-                left2.add(p);
-                left2.add(fracAdd(p, z));
+class Solution
+{
+    public boolean splitArraySameAverage (int[] nums)
+    {
+        int totSum = 0;
+        for (int num : nums)
+        {
+            totSum += num;
+        }
+        int len = nums.length;
+        int[][] next = new int[len + 1][totSum + 1];
+        for (int j=0;j<=len;j++)
+        {
+            for (int sum=0;sum<=totSum;sum++)
+            {
+                if (j > 0 && j < len)
+                {
+                    if (sum * (len - j) == (totSum - sum) * j)
+                    {
+                        next[j][sum] = 1;
+                    }
+                }
             }
-            left = left2;
         }
-
-        if (left.contains(new Point(0, 1))) return true;
-
-        Set<Point> right = new HashSet();
-        right.add(A2.get(N-1));
-        for (int i = N/2; i < N-1; ++i) {
-            Set<Point> right2 = new HashSet();
-            Point z = A2.get(i);
-            right2.add(z);
-            for (Point p: right) {
-                right2.add(p);
-                right2.add(fracAdd(p, z));
+        for (int ind=len-1;ind>=0;ind--)
+        {
+            int[][] curr = new int[len + 1][totSum + 1];
+            for (int count=len-1;count>=0;count--)
+            {
+                for (int sum=totSum-nums[ind];sum>=0;sum--)
+                {
+                    curr[count][sum] = next[count][sum] | next[count + 1][sum + nums[ind]];
+                }
             }
-            right = right2;
+            for (int i=0;i<=len;i++)
+            {
+                for (int j=0;j<=totSum;j++)
+                {
+                    next[i][j] = curr[i][j];
+                }
+            }
         }
-
-        if (right.contains(new Point(0, 1))) return true;
-
-        Point sleft = new Point(0, 1);
-        for (int i = 0; i < N/2; ++i)
-            sleft = fracAdd(sleft, A2.get(i));
-
-        Point sright = new Point(0, 1);
-        for (int i = N/2; i < N; ++i)
-            sright = fracAdd(sright, A2.get(i));
-
-        for (Point ha: left) {
-            Point ha2 = new Point(-ha.x, ha.y);
-            if (right.contains(ha2) && (!ha.equals(sleft) || !ha2.equals(sright)))
-                return true;
-        }
-        return false;
-    }
-
-    public Point fracAdd(Point A, Point B) {
-        int numer = A.x * B.y + B.x * A.y;
-        int denom = A.y * B.y;
-        int g = gcd(numer, denom);
-        numer /= g;
-        denom /= g;
-
-        if (denom < 0) {
-            numer *= -1;
-            denom *= -1;
-        }
-
-        return new Point(numer, denom);
-    }
-
-    public int gcd(int a, int b) {
-       if (b==0) return a;
-       return gcd(b, a%b);
+        return next[0][0] == 1;
     }
 }
