f4da6ffa910b1bd1ca7ba35aaf4ebaac50fc4834
BHARANIDHARAN K <bharanidharan5544@gmail.com>
2026-03-10 20:01:13 +0530

    Time: 1 ms (100%), Space: 46.6 MB (46.8%) - LeetHub

---
+++ 3034-number-of-subarrays-that-match-a-pattern-i/3034-number-of-subarrays-that-match-a-pattern-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countMatchingSubarrays(int[] nums, int[] pattern) {
+        int n = nums.length;
+        int m = pattern.length;
+        int count = 0;
+        for (int i = 0; i <= n - m - 1; i++) {
+            boolean match = true;
+            for (int k = 0; k < m; k++) {
+                if (pattern[k] == 1 && nums[i + k + 1] <= nums[i + k]) {
+                    match = false;
+                    break;
+                }
+                if (pattern[k] == 0 && nums[i + k + 1] != nums[i + k]) {
+                    match = false;
+                    break;
+                }
+                if (pattern[k] == -1 && nums[i + k + 1] >= nums[i + k]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) count++;
+        }
+        return count;
+    }
+}
