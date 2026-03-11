bf16f61892248f9970d1230860b9cb1606b304da
BHARANIDHARAN K <bharanidharan5544@gmail.com>
2026-03-11 18:48:50 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.33%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.c
@@ -0,0 +1,10 @@
+int bitwiseComplement(int num) {
+    if(num == 0) return 1;
+    int mask = num;
+    mask |= mask >> 1;
+    mask |= mask >> 2;
+    mask |= mask >> 4;
+    mask |= mask >> 8;
+    mask |= mask >> 16;
+    return num ^ mask;
+}
